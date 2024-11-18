054d70e348ca5b4976c81ebf0a8f5b92bd4fb8f4
Crusher90 <rahulvallecha17@gmail.com>
2024-11-18 19:41:47 +0530

    setup basic anim instance class

---
+++ Source/GAS/Private/Character/GAnimInstance.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/GAnimInstance.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UGAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	MyCharacter = Cast<ACharacter>(TryGetPawnOwner());	
+}
+
+void UGAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	if(MyCharacter == nullptr)
+		return;
+	FVector Velocity = MyCharacter->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed = Velocity.Size();
+
+	bIsInAir = MyCharacter->GetCharacterMovement()->IsFalling();
+	bIsAccelerating = MyCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false;
+}
---
+++ Source/GAS/Public/Character/GAnimInstance.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "GAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_API UGAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation|Attributes", meta = (AllowPrivateAccess = "true"))
+	ACharacter* MyCharacter;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation|Attributes", meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation|Attributes", meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation|Attributes", meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
+};
